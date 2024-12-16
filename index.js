65aeaa383b27ee1a9afab054e78ac2559c16a793
Emanuel Vogt <emanuel.vogt@pixit.com.br>
2024-12-16 12:35:34 -0300

    FIX CODE

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { SerialPort } = require('serialport')
+const { SerialPort } = require('serialport');
 
 const port = new SerialPort({
-  path: '/dev/ttyS3',
+  path: 'COM3',        // Defina a porta COM apropriada
   baudRate: 9600,
 });
 
 port.on('open', () => {
-  console.log('Porta serial aberta');
-  const mensagem = 'Olá, UART3!';
+  console.log('Porta serial aberta na COM4');
+  
+  // Envie uma mensagem assim que a porta abrir, se desejar
+  const mensagem = 'DE WINDOWNS PARA LINUX';
 
+  // Envia a mensagem a cada 5 segundos
   setInterval(() => {
     port.write(mensagem, (err) => {
       if (err) {
@@ -17,6 +20,11 @@ port.on('open', () => {
       console.log('Mensagem enviada:', mensagem);
     });
   }, 5000);
+});
+
+// Evento disparado quando dados são recebidos pela serial
+port.on('data', (data) => {
+  console.log('Dados recebidos:', data.toString());
 });
 
 port.on('error', (err) => {
